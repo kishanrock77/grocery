19c1568c0bc19a445a8dff23015515c191f25b63
kishan777R <softwaredeveloperkp@gmail.com>
2026-05-31 22:31:19 +0530

    f

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const Razorpay = require('razorpay');
-const adminfirebase =
+const admin =
   require('../firebase');
 const router = express.Router();
 const Wallet = require("../models/Wallet");
@@ -80,7 +80,7 @@ async function sendFCM({
       return;
     }
 
-    await adminfirebase
+    await admin
       .messaging()
       .send({
 
